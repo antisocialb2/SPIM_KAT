a0f515e917a43d78cb9fb80c487dabcc703bd25a
antisocialb2 <133562268+antisocialb2@users.noreply.github.com>
2024-07-17 21:16:12 +0700

    Update gamelist.lua

--- Game-list/gamelist.lua
+++ Game-list/gamelist.lua
@@ -1,3 +1,4 @@
 return {
     [621129760] = "https://raw.githubusercontent.com/antisocialb2/SPIM_KAT/main/Game-list/KAT.txt",
+    [286090429] = "https://raw.githubusercontent.com/antisocialb2/Spimine_ARSENAL/main/Spimine_ARSENAL.lua",
 }
