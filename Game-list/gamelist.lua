dc3cf6e22aab47ef595df33ee12913723b99bcdd
antisocialb2 <133562268+antisocialb2@users.noreply.github.com>
2024-07-19 14:29:37 +0700

    Update gamelist.lua

--- Game-list/gamelist.lua
+++ Game-list/gamelist.lua
@@ -2,4 +2,5 @@ return {
     [621129760] = "https://raw.githubusercontent.com/antisocialb2/SPIM_KAT/main/Game-list/KAT.txt",
     [286090429] = "https://raw.githubusercontent.com/antisocialb2/Spimine_ARSENAL/main/Spimine_ARSENAL.lua",
     [3101667897] = "https://raw.githubusercontent.com/antisocialb2/SPIM_KAT/main/Game-list/LegendofSpeed.lua",
+    [893973440] = "https://raw.githubusercontent.com/antisocialb2/SPIMINE-FLEETHEFACILITY/main/script.lua",
 }
